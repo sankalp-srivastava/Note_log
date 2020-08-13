8dc87c1fe6b876f442ae780754482e3add6b220d
Sankalp Srivastava <57250597+sankalp-srivastava@users.noreply.github.com>
2020-08-13 22:14:15 +0530

    Added read entry function
    started working on multiuser function

--- notez.py
+++ notez.py
@@ -20,27 +20,42 @@ def welcome():
     """ Welcome Screen Lines"""
     print("Press 1 To Write Entry")
     print("Press 2 To View Entry")
+    print("Press 3 To Exit")
     return input()
 
 
 def write_file(dat, note):
-    with open("Entry.txt", "a+") as _fil:
-        _fil.write('['+ str(dat) + ']' + '\n' + note + '\n')
+    try:
+        with open("Entry.txt", "a+") as _fil:
+            _fil.write('[' + str(dat) + ']' + '\n' + note + '\n')
+    except:  # except part not working properly
+        print("NO Entries found")
+
+
+def read_file():
+    with open("Entry.txt", "r") as _fil:
+        line = _fil.read()
+        print(line)
+
+def new_user(usrname):
+    with open("user.csv","a+") as file:
+        name = csv.writer(file,delimiter = "|")
+        ''' rest of the code'''
 
 
 def main():
     option = welcome()
     if option == '1':
         note = write_note()
-        dat = datetime.now()
-        write_file(dat,note)
+        dat = datetime.now()  # date and time needs to be formatted
+        write_file(dat, note)
+        main()
+    elif option == '2':
+        read_file()
+        main()
+    elif option == '3':
         sys.exit(0)
 
-
-def read_file():
-    with open("Entry.txt","r") as _fil:
-        line = _fil.read()
-        print(line)
 
 if __name__ == "__main__":
     main()
